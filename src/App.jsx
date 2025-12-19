dab53726459850ca991c8616716fa3e5f01ba041
Utkarsh Kumar <utkarsh kumar>
2025-12-19 13:07:46 +0000

    Initial Netflix CI/CD demo app

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ color: "red", fontSize: "40px", textAlign: "center", marginTop: "100px" }}>
+      NETFLIX CI/CD DEMO
+    </div>
+  );
+}
+
+export default App;
